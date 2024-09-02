7ec9f35571eb7fc8c93ff36fefde48eac7000585
samar1100 <pankajsamar14@gmail.com>
2024-09-02 05:36:57 +0530

    Add files via upload

---
+++ scripts/word-list.js
@@ -0,0 +1,90 @@
+const wordList = [
+    {
+        word: "malaria",
+        hint: "It is transmitted one person to another by female Anopheles mosquito."
+    },
+    {
+        word: "oxygen",
+        hint: "A colorless, odorless gas essential for life."
+    },
+    {
+        word: "pneumonia",
+        hint: "Spread by cough and sneezes, by sharing drinking glass."
+    },
+    {
+        word: "cold",
+        hint: "Infect the nose and respiratory passage but not the lungs."
+    },
+    {
+        word: "typhoid",
+        hint: "Sustained high fever,weakness,stomach pain,constipation,headacheand loss of appettite our common symptoms."
+    },
+    {
+        word: "allergy",
+        hint: "Exaggerated response of the immune system to certain antigens present in the enviornment."
+    },
+    {
+        word: "cancer",
+        hint: "One of the most dreaded diseases of human beings and is major cause of death all over the globe."
+    },
+    {
+        word: "opioide",
+        hint: "It bind to specific opioid receptors present in our CNS and gastrointestinal trat."
+    },
+    {
+        word: "cocaine",
+        hint: "Commonly cold crack."
+    },
+    {
+        word: "cannabinoids",
+        hint: "A commonly called ganja."
+    },
+    {
+        word: "rizobium",
+        hint: "Bacteria involved in nitrogen fixation."
+    },
+    {
+        word: "yeast",
+        hint: "Fungi used in baking industries."
+    },
+    {
+        word: "lipases",
+        hint: "Used in ditergent and form removal of oily stains."
+    },
+    {
+        word: "propases",
+        hint: "Used to clarify bottle juices."
+    },
+    {
+        word: "science",
+        hint: "The systematic study of the structure and behavior of the physical and natural world."
+    },
+    {
+        word: "weber",
+        hint: "Unit of magnatic flux."
+    },
+    {
+        word: "newton",
+        hint: "Absoulte unit of force."
+    },
+    {
+        word: "farad",
+        hint: "SI unit of capacitance."
+    },
+    {
+        word: "henery",
+        hint: "Unit of self inductance or mutual inductance."
+    },
+    {
+        word: "galaxy",
+        hint: "A vast system of stars, gas, and dust held together by gravity."
+    },
+    {
+        word: "hertz",
+        hint: "Measure the number of wwave cycles passing through a given point in a second."
+    },
+    {
+        word: "volcano",
+        hint: "A mountain or hill with a vent through which lava, rock fragments, hot vapor, and gas are ejected."
+    },
+]
